09a967e4b83425337c3b970c7ac6f9eb82a5bb0b
Pacha-PC\Pacha <victoralonsopacha@outlook.es>
2017-07-17 22:23:52 -0500

    cambio vista formulario-finalprestamo

--- BookAdmin-master2Author/proyectobookadmin_ParaPresentarSprint/bookadmin/bookadmin/org.SmarTech.GUI/FinalizarPrestamo.aspx.cs
+++ BookAdmin-master2Author/proyectobookadmin_ParaPresentarSprint/bookadmin/bookadmin/org.SmarTech.GUI/FinalizarPrestamo.aspx.cs
@@ -4,23 +4,37 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BookAdmin.org.SmarTech.entities;
+using BookAdmin.org.SmarTech.controllers;
+using BookAdmin.org.SmarTech.Bussines;
 
 namespace BookAdmin.org.SmarTech.GUI
 {
     public partial class FinalizarPrestamo : System.Web.UI.Page
     {
         private static string code;
+        private static string titlebook;
         private static string nameCustomer;
+        private static BsnClsBook bsn_book = new BsnClsBook();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            loadInformation();
+
+            
+        }
+
+        protected void loadInformation()
+        {
             code = Convert.ToString(Session["bookview"]);
-            textCodeLoan.Text = code;
+            textTitleBook.Text =code;
 
             nameCustomer = Convert.ToString(Session["idCustomer"]);
             textNameCustomer.Text = nameCustomer;
 
-            
+
+ 
         }
 
 
